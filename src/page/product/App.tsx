08c185396f680dfecba54d2afeb543fa82b9c7a5
李颖 <liying@hong5.com.cn>
2018-04-03 18:33:11 +0800

    product

--- src/page/product/App.tsx
+++ src/page/product/App.tsx
@@ -3,11 +3,23 @@ import './product.less';
 import Products from '../../components/products';
 
 class ProductTab extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {background: '', color: ''};
+    this.tab = this.tab.bind(this);
+  }
+  tab() {
+    console.log(this.state);
+    this.setState({
+      background: '#C30D23', color: '#fff'
+    });
+  }
   render() {
+    const style = this.state;
     return (
       <div className="product">
         <div className="pro-tab">
-          <p>亚磁热疗养生床垫</p>
+          <p onClick={this.tab} style={style}>亚磁热疗养生床垫</p>
           <p>3D气压全位按摩椅</p>
         </div>
         <Products />
