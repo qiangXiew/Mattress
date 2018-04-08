e44f3879a157d98261c842c753412c60112dcddc
谢强 <xieqiang@hong5.com.cn>
2018-04-08 16:10:24 +0800

    增加异步组件 axios

---
+++ src/lib/webApiConfig.tsx
@@ -0,0 +1,19 @@
+let NodeEnv = process.env.NODE_ENV || '';
+
+const baesUrl = {
+    development: {
+        path: 'http://192.168.0.50:8081'
+    },
+    production: {
+        path: 'http://192.168.0.50:8081'
+    }
+};
+
+const config = {
+    news: {
+        path: `${baesUrl[NodeEnv].path}/H5Website/findNewsPage`,
+        method: 'GET',
+        description: '新闻接口'
+    }
+};
+export default config;
